7629ce51841e7c7f0b9db2c680da22e62609e485
Vincent Xu <vincent.xu1@outlook.com>
2025-08-13 10:47:20 -0700

    Implemented generic Node class

---
+++ Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._3
+{
+    public class Node<T>(T data)
+    {
+        public T? Value { get; set; } = data;
+        public Node<T>? Previous { get; set; }
+        public Node<T>? Next { get; set; }
+        public override string? ToString() => Value?.ToString();
+    }
+}
