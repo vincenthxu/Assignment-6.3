da3103137f980c23da15603a705a6bdf1e4def38
Vincent Xu <vincent.xu1@outlook.com>
2025-08-13 10:49:47 -0700

    Added Main method that uses generic Queue and demonstrates Queue functionality

--- Program.cs
+++ Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<Customer> queue = new();
+
+            string[]? names = { null, "Vincent Xu", "John Cena" };
+
+            foreach(var name in names)
+            {
+                queue.Enqueue(new Customer { Name = name });
+            }
+
+            Console.WriteLine("Display():");
+            queue.Display();
+
+            Console.WriteLine($"Dequeue(): {queue.Dequeue()}");
+            Console.WriteLine("Display():");
+            queue.Display();
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            try
+            {
+                queue.Dequeue();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Display():");
+            queue.Display();
         }
     }
 }
