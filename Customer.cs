99189dec477db49b91850c6d78cedb1de662a80a
Vincent Xu <vincent.xu1@outlook.com>
2025-08-13 10:48:26 -0700

    Created simple Customer class

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Assignment_6._3
+{
+    public class Customer
+    {
+        public string? Name { get; set; }
+        public override string ToString() => JsonSerializer.Serialize(this);
+    }
+}
