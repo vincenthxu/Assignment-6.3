934cf9dad91b74a8eb06381bb4ef6e12466e144e
Vincent Xu <vincent.xu1@outlook.com>
2025-08-13 10:11:05 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_6._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
