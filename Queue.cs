7ea457f912a9ab84471f985c3d5833231ef93aa9
Vincent Xu <vincent.xu1@outlook.com>
2025-08-13 10:48:10 -0700

    Implemented generic Queue using generic LinkedLists

---
+++ Queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_6._3
+{
+    public class Queue<T>
+    {
+        private LinkedList<T> _queue = new();
+        public int Length { get { return _queue.Size; } }
+        public void Enqueue(T item) => _queue.AddLast(item);
+        public Node<T>? Dequeue() => _queue.RemoveFirst();
+        public void Display() => _queue.Display();
+    }
+}
